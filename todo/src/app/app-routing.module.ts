4959e2323f9039f1e3f8be655555b72b5e233036
Peter Burdette <peterburdette@gmail.com>
2022-01-13 12:05:04 -0500

    added about component

--- todo/src/app/app-routing.module.ts
+++ todo/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'list', component: ListComponent },
+  { path: 'about', component: AboutComponent },
 ];
 
 @NgModule({
